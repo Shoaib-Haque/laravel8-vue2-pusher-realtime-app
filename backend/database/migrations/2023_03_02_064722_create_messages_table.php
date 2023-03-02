2b0fe39b3e43af32bf4e3ba1728ad6ce09747839
shoaib <shoaibhaque7@gmail.com>
2023-03-02 12:50:20 +0600

    created migration files

---
+++ backend/database/migrations/2023_03_02_064722_create_messages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sender_id')->unsigned();
+            $table->foreign('sender_id')->references('id')->on('users');
+            $table->integer('receiver_id')->unsigned();
+            $table->foreign('receiver_id')->references('id')->on('users');
+            $table->text('message');
+            $table->dateTime('created_at', $precision = 0);
+            $table->dateTime('update_at', $precision = 0);
+        });
+
+        \DB::statement('ALTER TABLE messages AUTO_INCREMENT = 1;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
